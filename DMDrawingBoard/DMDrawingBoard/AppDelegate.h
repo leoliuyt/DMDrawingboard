8543bc0720451a13314a2d6451fa663449bc0ef2
lbq <lbq@LEO-MBP.local>
2017-09-04 14:20:51 +0800

    create project

---
+++ DMDrawingBoard/DMDrawingBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DMDrawingBoard
+//
+//  Created by lbq on 2017/9/4.
+//  Copyright © 2017年 lbq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
